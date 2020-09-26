1b96341e65921886f85e0de31b80fc0b507a5f3b
Weijing Lin <weijingjaylin@gmail.com>
2020-09-26 01:34:43 -0700

    display ip fix

--- index.js
+++ index.js
@@ -4,17 +4,19 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
+
   res.send('Hello World!')
 })
 
-(async () => {
-  console.log(await publicIp.v4());
-  //=> '46.5.21.123'
 
-  console.log(await publicIp.v6());
-  //=> 'fe80::200:f8ff:fe21:67cf'
-})();
 
 app.listen(port, () => {
+  (async () => {
+    console.log(await publicIp.v4());
+    //=> '46.5.21.123'
+  
+    console.log(await publicIp.v6());
+    //=> 'fe80::200:f8ff:fe21:67cf'
+  })();
   console.log(`Example app listening at http://localhost:${port}`)
 })
