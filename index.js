bc937bfff2bbc32532a699664ba31a60a8025997
Weijing Lin <weijingjaylin@gmail.com>
2020-09-26 01:32:24 -0700

    add ipAddress

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const express = require('express')
+const publicIp = require('public-ip');
 const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
+(async () => {
+  console.log(await publicIp.v4());
+  //=> '46.5.21.123'
+
+  console.log(await publicIp.v6());
+  //=> 'fe80::200:f8ff:fe21:67cf'
+})();
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
